4a8f587633abdb64cefef9b99ba68a216c9f3a74
oranleiba <leiba.oran@gmail.com>
2016-03-28 00:26:41 +0300

    initial ws connection, broadcasting neurosky output to client side ws

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
  * Created by oran on 3/20/2016.
  */
 "use strict";
-var neurosky = require("./lib/Neurosky");
+var neurosky = require("./lib/Neurosky"),
+    restServer = require("./lib/ExpressWrapper");
 
-module.exports = {
-    
-};
+var curServer = restServer.init({
+    port: 8080
+});
+
+var client = neurosky.createClient({
+    appName: 'NodeNeuroSky',
+    appKey: '0fc4141b4b45c675cc8d3a765b8d71c5bde9390'
+})
+
+client.on('data', function (data) {
+    var clients = restServer.getNSSockets();
+    clients.forEach(function each(client) {
+        client.send(JSON.stringify(data));
+    });
+});
+
+client.connect();
+
+module.exports = {};
